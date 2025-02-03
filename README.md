93d7210a68845c3ebf8aaa99836e02d3b4963d1a
riuteshyaaa <riteshyaaa@gmail.com>
2025-02-03 12:04:45 +0530

    Setup Tailwind CSS and DaisyUI, update App component with NavBar and styling

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - Created a vite + React project
 - Remove unnecessary code  and  create hello  world app 
+- Setup Tailwind css in my project
+- Install and setup DaisyUi 
+- Add NavBar component to App.jsx
+-  
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
 
-const App = () => {
+function App() {
     return (
-      <div>
-        <h1> Helloo World </h1>
-        </div>)
-}
-export default App;
+        <>
+        <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl"> 👩‍💻DevTinder</a>
+  </div>
+  <div className="flex-none gap-2">
+    
+    <div className="dropdown dropdown-end mx-5">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full ">
+          <img
+            alt="Tailwind CSS Navbar component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li>
+          <a className="justify-between">
+            Profile
+            <span className="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+      <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+        <h1 className="text-6xl font-bold text-black animate-bounce">
+          Hello World!
+        </h1>
+      </div>
+      </>
+    );
+  }
+  
+  export default App;
